6eb94dfb446c619032a5fc63008725efaed27da8
Maselkov <Maselkov@users.noreply.github.com>
2022-12-06 17:45:11 +0100

    Add d6

---
+++ d6p2.py
@@ -0,0 +1,12 @@
+from puzzleinput import lines
+from collections import deque
+
+size = 14
+signal = lines[0]
+buffer = deque(maxlen=size)
+for index, character in enumerate(signal, 1):
+    buffer.append(character)
+    if len(buffer) == size:
+        if len(set(buffer)) == size:
+            print(index)
+            break
